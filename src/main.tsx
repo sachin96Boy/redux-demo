85208a29a84bf0b89df431ec5cbda0d3a2c42c62
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-11-21 17:03:19 +0530

    init: provider and store

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+
+import { store } from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>
 );
