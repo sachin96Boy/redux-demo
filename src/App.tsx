7674d18c813aaad18217cb51e6d75a9cdde64f17
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-11-21 15:43:32 +0530

    init: vite react proj with chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <Box>
+      <Text>Hello world</Text>
+    </Box>
+  );
+}
+
+export default App;
